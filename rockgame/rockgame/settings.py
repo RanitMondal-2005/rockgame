6fcfd6629fc40827c7388e5e77c49fee89899e72
Ranit Mondal <mondalranit2002@gmail.com>
2026-02-26 02:20:31 +0530

    SINCE DEPLOYMENT FAILED, SO DEBUG TRUE AND ALLOWED HOST=[]

--- rockgame/rockgame/settings.py
+++ rockgame/rockgame/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_)!pi=!z*94=rvb1k8gg5_3ygp^9g2=55ff36#%wa!t-^t$q&r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["rockgame.onrender.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
